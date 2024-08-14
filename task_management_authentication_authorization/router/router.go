111411f623d68be8332f52d9aa88378230e4f0df
Ephrem <eph21sz@gmail.com>
2024-08-14 18:02:24 +0300

    Modify routing

--- task_management_authentication_authorization/router/router.go
+++ task_management_authentication_authorization/router/router.go
@@ -9,11 +9,11 @@ import (
 // func SetupRouter() *gin.Engine {
 // 	router := gin.Default()
 
-// 	router.GET("/tasks", controllers.GetTasks)
-// 	router.GET("/tasks/:id", controllers.GetTaskByID)
-// 	router.POST("/tasks", controllers.CreateTask)
-// 	router.PUT("/tasks/:id", controllers.UpdateTask)
-// 	router.DELETE("/tasks/:id", controllers.DeleteTask)
+// router.GET("/tasks", controllers.GetTasks)
+// router.GET("/tasks/:id", controllers.GetTaskByID)
+// router.POST("/tasks", controllers.CreateTask)
+// router.PUT("/tasks/:id", controllers.UpdateTask)
+// router.DELETE("/tasks/:id", controllers.DeleteTask)
 
 // 	return router
 // }
